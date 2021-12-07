b108acfd589ff96932778b22bdf67d16cab1dd90
Vitaly Borisov <vitaliy.borisov@intspirit.com>
2021-12-07 15:00:30 +0300

    init

---
+++ angular/src/app/login/login.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent implements OnInit {
+  @ViewChild('loginform', { static: false }) loginForm!: NgForm;
+
+  constructor(
+    private http: HttpClient,
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  onLoginSubmit() {
+    const username = this.loginForm.value.username;
+    const password = this.loginForm.value.password;
+
+    const headers = new HttpHeaders({ 'Content-type': 'application/json' });
+
+    const reqObject = {
+      username: username,
+      password: password,
+    };
+
+    this.http
+      .post('http://localhost:3000/users/login', reqObject, {
+        headers: headers,
+      })
+      .subscribe(
+        (response) => {
+          this.authService.setLocalStorage(response);
+        },
+        (error) => {
+          console.log(error);
+        },
+        () => {
+          console.log('done!');
+          this.router.navigate(['protected']);
+        }
+      );
+  }
+
+  ngOnInit() {}
+}
---
+++ angular/src/app/register/register.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css'],
+})
+export class RegisterComponent implements OnInit {
+  @ViewChild('registerform', { static: false })
+  registerForm!: NgForm;
+
+  constructor(private http: HttpClient, private router: Router) {}
+
+  ngOnInit() {}
+
+  // Submits a post request to the /users/register route of our Express app
+  onRegisterSubmit() {
+    const username = this.registerForm.value.username;
+    const password = this.registerForm.value.password;
+
+    const headers = new HttpHeaders({ 'Content-type': 'application/json' });
+
+    const reqObject = {
+      username: username,
+      password: password,
+    };
+
+    this.http
+      .post('http://localhost:3000/users/register', reqObject, {
+        headers: headers,
+      })
+      .subscribe(
+        (response) => {
+          console.log(response);
+        },
+
+        (error) => {
+          console.log(error);
+        },
+
+        () => {
+          console.log('done!');
+          this.router.navigate(['login']);
+        }
+      );
+  }
+}
